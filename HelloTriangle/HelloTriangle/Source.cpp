dd27ff94868da63606240154a98197b42f9d959a
PatrickWendling <patrick.bw9@gmail.com>
2023-10-09 13:20:15 -0300

    Lista 1 - 5d

--- HelloTriangle/HelloTriangle/Source.cpp
+++ HelloTriangle/HelloTriangle/Source.cpp
@@ -103,6 +103,13 @@ int main()
 
 		// Chamada de desenho - drawcall
 		// Poligono Preenchido - GL_TRIANGLES
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		// Adição das demais formas e suas cores
+		shader.setVec4("inputColor", 0.0f, 1.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_LINE_LOOP, 0, 6);
+
+		shader.setVec4("inputColor", 1.0f, 0.0f, 1.0f, 1.0f);
 		glDrawArrays(GL_POINTS, 0, 6);
 
 		//Desconectando o buffer de geometria
